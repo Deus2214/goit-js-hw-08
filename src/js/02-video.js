e4918c30f165fa7784fe62e0d240b125c44c08b5
Vitalii Gritsun <110825559+Deus2214@users.noreply.github.com>
2022-12-26 13:55:44 +0300

    Дз виконано

---
+++ src/js/02-video.js
@@ -0,0 +1,21 @@
+import Player from '@vimeo/player';
+import {throttle} from 
+'throttle-debounce';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const savedTimeLocalStorage = +localStorage.getItem('videoplayer-current-time');
+
+if (savedTimeLocalStorage) {
+  player.setCurrentTime(savedTimeLocalStorage);
+}
+
+player.on('timeupdate', throttle(1000, onPlay));
+
+function onPlay(data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import { throttle } from 'throttle-debounce';
+
+// виборка елементів
+const form = document.querySelector('.js-feedback-form');
+
+// навішування прослуховувачів
+form.addEventListener('input', throttle(500, onLocalStorageSet));
+form.addEventListener('submit', onFormSubmit);
+
+// код
+lastFormInput();
+ // функція відновлює останні дані, що було до перезавантаження сторінки
+
+// сет функцій
+function onLocalStorageSet(evt) {
+  let savedData = localStorage.getItem('feedback-form-state');
+  savedData = savedData ? JSON.parse(savedData) : {}; 
+  //перевіряємо якщо дані в LocalStorafe є, то перезаписуємо їх, якщо немає, створюємо обєкт з такими властивостями
+  savedData[evt.target.name] = evt.target.value; 
+  //зберігаємо введені дані як властивість у об'єкт
+  localStorage.setItem('feedback-form-state', JSON.stringify(savedData)); //записуємо отримані дані в локал Сторедж
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const formData = new FormData(form);
+  formData.forEach((value, name) => console.log(name, value));
+
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function lastFormInput() {
+  let savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    savedData = JSON.parse(savedData);
+    Object.entries(savedData).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
